bb5daa8137bfdebd6ed4ea34d4733c10b6d5da80
Piyush Priyadarshi Personal laptop WSL <priyadarship85@gmail.com>
2022-01-09 11:01:44 +0530

    updated backup scripts

--- src/backup.js
+++ src/backup.js
@@ -4,7 +4,7 @@ import './scheduler.js';
 
 async function startProcess() {
   try {
-      console.log("Bacup Scripts Running in Backgroun...");
+      console.log("Bacup Scripts Running in Backgroun... "+ new Date().toLocaleTimeString());
   } catch (error) {
     console.log(error);
   }
